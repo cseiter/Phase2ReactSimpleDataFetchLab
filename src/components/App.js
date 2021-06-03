b61e05fc26ba8e3598acdfab93c2ab2974d44b0e
chris <cseiter@gmail.com>
2021-06-03 19:25:29 -0400

    "Loading..." appears on screen, does not return fetch results

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import React,{useState,useEffect} from "react";
 
 function App() {
     const [getRandDog, setRandDog] = useState([]);
+    const [isLoaded,setIsLoaded] = useState(false);
 
     useEffect(() => {
         fetch("https://dog.ceo/api/breeds/image/random")
@@ -11,6 +12,8 @@ function App() {
             setRandDog(data.message);
         });
     },[]);
+
+    if (!isLoaded) return <p>Loading...</p>;
     return <div id="getRandDog"><img src="getRandDog" /></div>
 }
 
