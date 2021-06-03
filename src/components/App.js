cc7eabaf0c820f81394d176734c03d6f76bf8cd3
chris <cseiter@gmail.com>
2021-06-03 19:21:56 -0400

    write out App component using prev lesson as example

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,17 @@
 // create your App component here
+import React,{useState,useEffect} from "react";
+
+function App() {
+    const [getRandDog, setRandDog] = useState([]);
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then((response) => response.json())
+        .then((data) => {
+            setRandDog(data.message);
+        });
+    },[]);
+    return <div id="getRandDog"><img src="getRandDog" /></div>
+}
+
+export default App;
